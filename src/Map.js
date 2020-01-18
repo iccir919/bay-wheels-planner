789ebfed836d48c518e752e59cee3c63085715a9
Neil Ricci <iccir919@gmail.com>
2020-01-18 11:11:48 -0800

    Add map and header

---
+++ src/Map.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import GoogleMapReact from 'google-map-react';
+import config from './config';
+
+class Map extends Component {
+  static defaultProps = {
+    center: {
+      lat: 59.95,
+      lng: 30.33
+    },
+    zoom: 11
+  };
+
+  render() {
+    return (
+      // Important! Always set the container height explicitly
+      <div style={{ height: '100vh', width: '100%' }}>
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: config.GOOGLE_MAPS_API_KEY }}
+          defaultCenter={this.props.center}
+          defaultZoom={this.props.zoom}
+        >
+        </GoogleMapReact>
+      </div>
+    );
+  }
+}
+
+export default Map;
