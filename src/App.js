1e66b824f80e41dd7d8bcddd1add0e814e8cfaca
Neil Ricci <iccir919@gmail.com>
2020-02-08 16:03:29 -0800

    Reinsert logic for fetching live data

--- src/App.js
+++ src/App.js
@@ -6,14 +6,35 @@ class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            
             routes: null,
-            bikeType: 'free'
+            bikeType: 'free',
+            stationStatus: [],
+            freeBikeStatus : [],
+            stationInfo: []
         }
         this.handleInputChange = this.handleInputChange.bind(this);
     }
 
     componentDidMount() {
+        this.systemStatusUpdate();
+    }
+
+    systemStatusUpdate() {
+        Promise.all([
+          fetch('https://gbfs.baywheels.com/gbfs/en/station_information.json'), 
+          fetch('https://gbfs.baywheels.com/gbfs/en/station_status.json'),
+          fetch('https://gbfs.baywheels.com/gbfs/en/free_bike_status.json')
+        ])
+        .then(([stationInfo, stationStatus, freeBikeStatus]) => { 
+           return Promise.all([stationInfo.json(), stationStatus.json(), freeBikeStatus.json()]) 
+        })
+        .then(([stationInfo, stationStatus, freeBikeStatus]) => {
+          this.setState({
+            stationInfo: stationInfo.data.stations,
+            stationStatus: stationStatus.data.stations,
+            freeBikeStatus: freeBikeStatus.data.bikes
+          })
+        });
     }
 
     handleInputChange(event) {
